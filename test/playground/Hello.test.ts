dc9207ad5bbc1d938b0559b8eafcc4be1798bf23
Josh Hill <hillj38@gene.com>
2021-08-18 14:10:06 -0500

    6.37

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,4 +1,7 @@
 import { handler } from '../../services/SpacesTable/Read';
 
 
-handler({} as any, {} as any);
+const result = handler({} as any, {} as any).then((apiResult) => {
+    const items = JSON.parse(apiResult.body);
+    console.log(123);
+});
