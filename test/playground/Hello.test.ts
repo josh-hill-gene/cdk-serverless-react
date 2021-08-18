eec27bb09d648a1ac0cdd4c0dac5609f6b323094
Josh Hill <hillj38@gene.com>
2021-08-18 15:04:01 -0500

    6.38

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,7 +1,14 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
 import { handler } from '../../services/SpacesTable/Read';
 
+const event: APIGatewayProxyEvent = {
+    queryStringParameters: {
+        spaceId: '5571c78e-c888-4805-bb09-607b55d7d09c'
+    },
+} as any;
 
-const result = handler({} as any, {} as any).then((apiResult) => {
+const result = handler(event, {} as any).then((apiResult) => {
+    console.log(apiResult);
     const items = JSON.parse(apiResult.body);
-    console.log(123);
+    console.log(items);
 });
