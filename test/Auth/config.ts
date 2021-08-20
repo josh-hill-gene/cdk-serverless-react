8d6d42f0793a7c3510e2e2f48c7615db55b3c5ed
Josh Hill <hillj38@gene.com>
2021-08-20 12:33:51 -0500

    finished section 8

--- test/Auth/config.ts
+++ test/Auth/config.ts
@@ -2,7 +2,7 @@ export const config = {
     REGION: 'us-east-1',
     USER_POOL_ID: 'us-east-1_3bcv6yKCp',
     APP_CLIENT_ID: '732m40ajkumarug63mgnph34ku',
-    IDENTITY_POOL_ID: '',
+    IDENTITY_POOL_ID: 'us-east-1:303323cc-bf3e-4128-98b0-a9b62994ab3f',
     TEST_USER_NAME: 'jlhill',
     TEST_USER_PASSWORD: 'Ch33syrice!',
 };
